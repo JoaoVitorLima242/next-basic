73066ebc51a05646c98070b8207e22ceedbeb746
JoaoVitorLima242 <jlima0500@gmail.com>
2022-05-11 10:41:01 -0300

    seeing client side render

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,27 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import { GetServerSideProps } from "next";
+import { useEffect, useState } from "react"
 
-export default function Home() {
+export default function Home({repos}) {
+  const [repositories, setRepositories]= useState<string[]>([])
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/JoaoVitorLima242/repos')
+      .then((response) => response.json())
+      .then((data) => {
+        const repoNames = data?.map(item => item.name); 
+
+        setRepositories(repoNames);
+      });
+  }, [])
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <ul>
+        {repositories.map((repo) => {
+          return <li key={repo}>{repo}</li>
+        })}
+      </ul>
     </div>
   )
 }
+
